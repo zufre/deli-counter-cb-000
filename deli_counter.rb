34571b6c1afd80b06a79885604cc6465d0fd12c7
Cedric David <cedric.david@btopenworld.com>
2018-09-02 01:50:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,33 @@
 # Write your code here.
 
-
-def line(array)
-  if array.size == 0
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
-    message = "The line is currently:"
-    array.each_with_index do |names, index|
-      message += " #{index+1}. #{names}"
+    current_line = "The line is currently:"
+    deli.each_with_index do |name, index|
+      current_line << " #{index +1}. #{name}"
     end
-    puts message
+
+    puts current_line
   end
+
+
 end
 
-def take_a_number(array, name)
-  position = array.size + 1
-  array.push(name)
-  puts "Welcome, #{name}. You are number #{position} in line."
+def take_a_number(deli, name)
+  deli << name
+  number = deli.length
+  puts "Welcome, #{name}. You are number #{number} in line."
+
 end
 
-def now_serving(array)
-  if array.size == 0
+def now_serving(deli)
+  if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-     person = array[0]
-     array.shift
-     puts "Currently serving #{person}."
+    puts "Currently serving #{deli[0]}."
+    deli.shift
   end
+
 end
