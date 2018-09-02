fff13f91e3bfd14ab1d8d8141f21997f3b931326
Cedric David <cedric.david@btopenworld.com>
2018-09-02 01:33:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,12 @@
 def line(deli)
   deli.empty?
   puts "The line is currently empty."
+
 end
+
+def take_an_number(deli, name)
+  deli << name 
+  deli.each_with_index do |name|
+    index +=1
+    puts "#{name} #{index}"
+  end
